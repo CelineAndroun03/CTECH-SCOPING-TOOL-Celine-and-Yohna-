5ba7e14a1ad69a57770f1e569c8984b2c077fca5
Androun <105802@global.ul.com>
2025-09-02 16:08:55 -0500

    Progress I have made so far 9/2

--- Code/1. Exploratory Data Analysis/EDA.py
+++ Code/1. Exploratory Data Analysis/EDA.py
@@ -6,22 +6,90 @@
 #data = pd.read_excel("Data/EDA Data V0.xlsx")
 #print(data.head(10))
 
+##################################################################
+    #1) Load the Excel dataset
+##################################################################
+
 # 1: Import Libraries
+from scipy.stats import skew
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # 2: Download Data
 data = pd.read_csv("Data_EXPLORATION.csv")
-#Prints (rows,columns)
-print("Shape:", data.shape)
+print("Shape:", data.shape) #Prints (rows,columns)
+
+##################################################################
+    #2) Missing Percentage / Distribution type 
+##################################################################
 
 missing_count = data.isna().sum() #Finds the sum of N/A values for each column
 missing_percent = (data.isnull().mean()*100).round(2) #Finds the percentage of N/A values for each column
 
+distribution = {} #THIS IS TO BE DETERMINED, IT IS NOT WORKING RIGHT NOW... I WILL MESSAGE U ABOUT IT
+for col in data.columns: #Loops through every column name in the dataset
+    if data[col].dropna().empty: #drop the missing values, and check if anything is left
+        distribution[col] = "No data" #If nothing is left, No data
+    elif data[col].dtype == "object": #
+        distribution[col] = "Categorical"
+    else:
+        s = skew(data[col].dropna()) #Selects the column and removes missing values
+        if s > 1:
+            distribution[col] = "Right skewed" #skew is positive
+        if s < -1:
+            distribution[col] = "Left skewed" #skew is negative
+        else:
+            distribution[col] = "Normal"
+
 #Table for % of missing values
 summary = pd.DataFrame({
         "Missing Count": missing_count, #Total # of missing values per column
-        "Missing %": missing_percent    #Percentage of missing values per column
+        "Missing %": missing_percent,    #Percentage of missing values per column
+        "  Distribution": distribution
         }).sort_values("Missing %", ascending=False) #Sorts the values in ascending order
 
-print("\nTOTAL % OF MISSING VALUES PER ATTRIBUTE")
+print("\nMISSING VALUES PER ATTRIBUTE + DISTRIBUTION")
 print(summary.to_string())
+
+##################################################################
+    #3) Data Visualization
+##################################################################
+data["total_actual_hrs"] = pd.to_numeric(data["total_actual_hrs"], errors="coerce")
+data["lab_actual_hrs"] = pd.to_numeric(data["lab_actual_hrs"], errors="coerce")
+data["eng_actual_hrs"] = pd.to_numeric(data["eng_actual_hrs"], errors="coerce")
+
+# 1) Plot the frequency of each investigation type
+plt.figure(figsize = (6,6))
+data["type_of_investigation"].value_counts().plot(kind = "bar")
+ax = data["type_of_investigation"].value_counts().plot(kind="bar", figsize=(6,6))
+for container in ax.containers:
+    ax.bar_label(container)
+plt.show()
+
+#2)  Plot the average of actual hours for each of these types (total, lab, eng)
+
+#Average total actual hours per project by investigation type
+avg_hours = data.groupby("type_of_investigation") ["total_actual_hrs"].mean()
+ax = avg_hours.plot(kind="bar")
+ax.bar_label(ax.containers[0])
+plt.show()
+
+#Average actual lab hours per project by investigation type
+avg_labHours = data.groupby("type_of_investigation") ["lab_actual_hrs"].mean()
+ax1 = avg_labHours.plot(kind="bar")
+ax1.bar_label(ax1.containers[0])
+plt.show()
+
+#Average actual eng hours per project by investigation type
+avg_engHours = data.groupby("type_of_investigation") ["eng_actual_hrs"].mean()
+ax2 = avg_engHours.plot(kind="bar")
+ax2.bar_label(ax2.containers[0])
+plt.show()
+
+
+
+
+
+
+
