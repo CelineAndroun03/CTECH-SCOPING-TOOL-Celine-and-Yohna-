e9e4eb8a678c8eeae4d3bc0539c59f1b881b4519
Androun <105802@global.ul.com>
2025-09-02 08:22:43 -0500

    Missing values summary

--- Code/1. Exploratory Data Analysis/EDA.py
+++ Code/1. Exploratory Data Analysis/EDA.py
@@ -1,8 +1,27 @@
 # 1: Import Libraries
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 
 # 2: Download Data
-data = pd.read_excel("Data/EDA Data V0.xlsx")
-print(data.head(10))
+#data = pd.read_excel("Data/EDA Data V0.xlsx")
+#print(data.head(10))
 
+# 1: Import Libraries
+import pandas as pd
+
+# 2: Download Data
+data = pd.read_csv("Data_EXPLORATION.csv")
+#Prints (rows,columns)
+print("Shape:", data.shape)
+
+missing_count = data.isna().sum() #Finds the sum of N/A values for each column
+missing_percent = (data.isnull().mean()*100).round(2) #Finds the percentage of N/A values for each column
+
+#Table for % of missing values
+summary = pd.DataFrame({
+        "Missing Count": missing_count, #Total # of missing values per column
+        "Missing %": missing_percent    #Percentage of missing values per column
+        }).sort_values("Missing %", ascending=False) #Sorts the values in ascending order
+
+print("\nTOTAL % OF MISSING VALUES PER ATTRIBUTE")
+print(summary.to_string())
