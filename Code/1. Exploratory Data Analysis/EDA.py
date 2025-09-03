4ddef32b74a45ceeff66dcca84c086b0a8d3109b
Androun <105802@global.ul.com>
2025-09-03 16:14:47 -0500

    Progress I have made so far 9/3

--- Code/1. Exploratory Data Analysis/EDA.py
+++ Code/1. Exploratory Data Analysis/EDA.py
@@ -1,37 +1,42 @@
-# 1: Import Libraries
-#import pandas as pd
-#import numpy as np
+# # 1: Import Libraries
+# #import pandas as pd
+# #import numpy as np
 
-# 2: Download Data
-#data = pd.read_excel("Data/EDA Data V0.xlsx")
-#print(data.head(10))
+# # 2: Download Data
+# #data = pd.read_excel("Data/EDA Data V0.xlsx")
+# #print(data.head(10))
 
-##################################################################
-    #1) Load the Excel dataset
-##################################################################
+# ##################################################################
+#     #1) Load the Excel dataset
+# ##################################################################
 
 # 1: Import Libraries
 from scipy.stats import skew
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas.api.types as ptypes
 
 # 2: Download Data
 data = pd.read_csv("Data_EXPLORATION.csv")
 print("Shape:", data.shape) #Prints (rows,columns)
 
-##################################################################
-    #2) Missing Percentage / Distribution type 
-##################################################################
+# ##################################################################
+#     #2) Missing Percentage / Distribution type 
+# ##################################################################
+data["total_actual_hrs"] = pd.to_numeric(data["total_actual_hrs"], errors="coerce")
+data["lab_actual_hrs"] = pd.to_numeric(data["lab_actual_hrs"], errors="coerce")
+data["eng_actual_hrs"] = pd.to_numeric(data["eng_actual_hrs"], errors="coerce")
+data["total_eng_hrs"] = pd.to_numeric(data["total_eng_hrs"], errors="coerce")
 
 missing_count = data.isna().sum() #Finds the sum of N/A values for each column
-missing_percent = (data.isnull().mean()*100).round(2) #Finds the percentage of N/A values for each column
+missing_percent = (data.isna().mean()*100).round(2) #Finds the percentage of N/A values for each column
 
-distribution = {} #THIS IS TO BE DETERMINED, IT IS NOT WORKING RIGHT NOW... I WILL MESSAGE U ABOUT IT
+distribution = {} 
 for col in data.columns: #Loops through every column name in the dataset
     if data[col].dropna().empty: #drop the missing values, and check if anything is left
         distribution[col] = "No data" #If nothing is left, No data
-    elif data[col].dtype == "object": #
+    elif not ptypes.is_numeric_dtype(data[col]):
         distribution[col] = "Categorical"
     else:
         s = skew(data[col].dropna()) #Selects the column and removes missing values
@@ -40,10 +45,11 @@ for col in data.columns: #Loops through every column name in the dataset
         if s < -1:
             distribution[col] = "Left skewed" #skew is negative
         else:
-            distribution[col] = "Normal"
+            distribution[col] = "Normal" #bell curve
 
 #Table for % of missing values
 summary = pd.DataFrame({
+        "Data Type" : data.dtypes.astype(str), #Type of data
         "Missing Count": missing_count, #Total # of missing values per column
         "Missing %": missing_percent,    #Percentage of missing values per column
         "  Distribution": distribution
@@ -55,11 +61,9 @@ print(summary.to_string())
 ##################################################################
     #3) Data Visualization
 ##################################################################
-data["total_actual_hrs"] = pd.to_numeric(data["total_actual_hrs"], errors="coerce")
-data["lab_actual_hrs"] = pd.to_numeric(data["lab_actual_hrs"], errors="coerce")
-data["eng_actual_hrs"] = pd.to_numeric(data["eng_actual_hrs"], errors="coerce")
 
-# 1) Plot the frequency of each investigation type
+
+#Plot the frequency of each investigation type
 plt.figure(figsize = (6,6))
 data["type_of_investigation"].value_counts().plot(kind = "bar")
 ax = data["type_of_investigation"].value_counts().plot(kind="bar", figsize=(6,6))
@@ -67,7 +71,7 @@ for container in ax.containers:
     ax.bar_label(container)
 plt.show()
 
-#2)  Plot the average of actual hours for each of these types (total, lab, eng)
+#lot the average of actual hours for each of these types (total, lab, eng)
 
 #Average total actual hours per project by investigation type
 avg_hours = data.groupby("type_of_investigation") ["total_actual_hrs"].mean()
@@ -87,9 +91,6 @@ ax2 = avg_engHours.plot(kind="bar")
 ax2.bar_label(ax2.containers[0])
 plt.show()
 
-
-
-
-
-
-
+##################################################################
+#4) correlation between the variables and the targets
+##################################################################
